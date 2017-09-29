2c2c26b46e83584e622dfb4af03fbd9021ed5023
Babrovskis <roberts.babrovskis@accenture.com>
2017-09-29 09:26:58 +0300

    Removed Tns names folder from version control

--- server_src/environment-setup.js
+++ server_src/environment-setup.js
@@ -1,32 +1,43 @@
 //add properties to environment path
 
-var path = require('path');
-process.env['PATH'] = path.join(__dirname,'..', '/instantclient') + ';' + process.env['PATH'];
-process.env['TNS_ADMIN']=path.join(__dirname,'..', '/tnsNames');
+const path = require('path');
+const settings = require('electron-settings');
+const INSTANT_CLIENT_SETTINGS_NAME = 'instantclient_path';
+const TNSNAMES_FOLDER_SETTINGS_NAME = 'tnsnames_path';
+const LOGS_FOLDER_SETTINGS_NAME = 'logs_path';
+const LOGGING_LEVEL_SETTTINGS_NAME = 'logs_level';
 
 
 function getLogFileName(){
     return `${new Date().toISOString().substring(0, 10)}_log.log`;
 }
 
+function getLogsFilesFolderPath(){
+    return settings.get(LOGS_FOLDER_SETTINGS_NAME, path.join(__dirname, '..', '/logs/'));
+}
+
 function createLogsDir(){
     var fs = require('fs');
-    var dir = path.join(__dirname, '..', `/logs`);
+    var dir = getLogsFilesFolderPath();
 
     if (!fs.existsSync(dir)){
+        log.info('createLogsDir: logs folder : '+ dir+ ' does not exist so creating it!');
         fs.mkdirSync(dir);
     }
 }
 
+//sets path to Oracle Instant client
+// presetDefaultSettingsValueIfNeeded(INSTANT_CLIENT_SETTINGS_NAME);
+process.env['PATH'] = settings.get(INSTANT_CLIENT_SETTINGS_NAME, path.join(__dirname,'..', '/instantclient')) + ';' + process.env['PATH'];
+
+//sets path to folder which contains tnsNames.ora file
+process.env['TNS_ADMIN']=settings.get(TNSNAMES_FOLDER_SETTINGS_NAME, path.join(__dirname,'..', '/tnsNames'));
+
 //configure logging
-var log = require('electron-log');
-log.transports.file.level = 'info';
+const log = require('electron-log');
+log.transports.file.level = settings.get(LOGGING_LEVEL_SETTTINGS_NAME, 'info');;
 
 createLogsDir();
 
-log.transports.file.file = path.join(__dirname, '..', `/logs/${getLogFileName()}`);
-// log.info('_____________________');
-// log.info('Current environment variables setup: ');
-// log.info(process.env);
-// log.info('_____________________');
+log.transports.file.file = path.join(getLogsFilesFolderPath(),getLogFileName());
 
