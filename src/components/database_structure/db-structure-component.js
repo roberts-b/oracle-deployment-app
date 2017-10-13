b90cd280f474baccf98e963c1e1669de2db5a507
Babrovskis <roberts.babrovskis@accenture.com>
2017-10-13 18:10:45 +0300

    Implemented db structure component

--- src/components/database_structure/db-structure-component.js
+++ src/components/database_structure/db-structure-component.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 const { ipcRenderer } = require('electron');
-import { Container, Accordion, Menu, Label, Grid, Divider, Segment } from 'semantic-ui-react';
+import { Container, Accordion, Menu, Label, Grid, Segment, Icon } from 'semantic-ui-react';
 import Constants from '../../constants/constants.js';
 import rpcNames from '../../constants/rpc-names.js';
 import PropTypes from 'prop-types';
 var log = require('electron-log');
+import { List } from 'react-virtualized';
 
 class DbStructureComponent extends React.Component {
 
@@ -14,26 +15,6 @@ class DbStructureComponent extends React.Component {
 
         this.state = {
             structureItemsArray: [],
-            // structureItemsArray: [{
-            //     itemName: 'View',
-            //     active: false,
-            //     itemValues: [{
-            //         name: 'Statistics'
-            //     },
-            //     {
-            //         name: 'Recourse'
-            //     }]
-            // },
-            // {
-            //     itemName: 'Table',
-            //     active: false,
-            //     itemValues: [{
-            //         name: 'Statistics Table'
-            //     },
-            //     {
-            //         name: 'Recourse Table'
-            //     }]
-            // }],
             isLoading: false
         }
     }
@@ -53,7 +34,7 @@ class DbStructureComponent extends React.Component {
     render() {
         return (
             <Container fluid>
-                <Segment compact basic loading={this.state.isLoading}>
+                <Segment compact className='structureComponentSegmentAroundAccordion' basic loading={this.state.isLoading}>
                     <Accordion styled as={Menu} vertical fluid>
                         {this.state.structureItemsArray.map((value, i) => {
                             return <Menu.Item key={i}>
@@ -62,17 +43,35 @@ class DbStructureComponent extends React.Component {
                                     index={i} active={value.active}>
 
                                 </Accordion.Title>
-                                <Accordion.Content active={value.active}>
-                                    <Grid celled>
-                                        {
-                                            value.itemValues.map((subValue, j) => {
-                                                return <Grid.Row key={j}>
-                                                    <Grid.Column className='dbStructureGridColumn' textAlign='right'>
-                                                        <Menu.Item as='a' key={j}>{subValue.name}</Menu.Item>
-                                                    </Grid.Column>
-                                                </Grid.Row>
-                                            })}
-                                    </Grid>
+                                <Accordion.Content className='bdStructureAccordionContent' active={value.active}>
+                                    <List
+                                        width={300}
+                                        height={400}
+                                        rowCount={value.itemValues.length}
+                                        rowHeight={40}
+                                        rowRenderer={
+                                            ({
+                                                key,         // Unique key within array of rows
+                                                index,       // Index of row within collection
+                                                isScrolling, // The List is currently being scrolled
+                                                isVisible,   // This row is visible within the List (eg it is not an overscanned row)
+                                                style        // Style object to be applied to row (to position it)
+                                            }) => {
+                                                return (
+                                                    <div key={key} style={style}>
+                                                        <Segment className='segmentAroundMenuItem' key={key}>
+
+                                                            <Menu.Item active={value.itemValues[index].active} as='a' key={key}>
+                                                                <Icon color={value.itemValues[index].valid === 'VALID' ? 'green' : 'red'}
+                                                                    name={value.itemValues[index].valid === 'VALID' ? 'check circle' : 'remove circle'} />
+                                                                {value.itemValues[index].name}
+                                                            </Menu.Item>
+                                                        </Segment>
+                                                    </div>
+                                                )
+                                            }
+                                        }
+                                    />
                                 </Accordion.Content>
                             </Menu.Item>
                         })}
@@ -93,19 +92,21 @@ class DbStructureComponent extends React.Component {
         log.info('handleAccordionTitleClick titleProps: ', index);
         let structureItemsArray = this.state.structureItemsArray;
         const active = structureItemsArray[index].active;
-        structureItemsArray[index].active = !active;
 
-        //fetch all objects if active is true and currently there is no items inside
-        if(structureItemsArray[index].active && structureItemsArray[index].itemValues.length === 0){
-            //fetch values
-            this.setState({isLoading: true});
-            log.info('isLoading: ',this.state.isLoading);
-            ipcRenderer.send(rpcNames.GET_ALL_OBJECTS_BY_OBJECT_TYPE.reqName,structureItemsArray[index].itemName);
-            
-        }else if(!structureItemsArray[index].active){
-            log.info('clearing itemValues array for ', structureItemsArray[index].itemName, ' because it was closed');
-            structureItemsArray[index].itemValues = [];
-        }
+        structureItemsArray[index].active = !active;
+        
+                //fetch all objects if active is true and currently there is no items inside
+                if(structureItemsArray[index].active && structureItemsArray[index].itemValues.length === 0){
+                    //fetch values
+                    this.setState({isLoading: true});
+                    log.info('isLoading: ',this.state.isLoading);
+                    ipcRenderer.send(rpcNames.GET_ALL_OBJECTS_BY_OBJECT_TYPE.reqName,structureItemsArray[index].itemName);
+                    
+                }else if(!structureItemsArray[index].active){
+                    log.info('clearing itemValues array for ', structureItemsArray[index].itemName, ' because it was closed');
+                    structureItemsArray[index].itemValues = [];
+                }
+        
 
         this.setState({ structureItemsArray: structureItemsArray });
     }
@@ -113,7 +114,7 @@ class DbStructureComponent extends React.Component {
     registerIpcListeners() {
         ipcRenderer.on(rpcNames.GET_UNIQUE_OBJECT_TYPES.respName, (event, arg) => {
             log.info('ipcRenderer.on(rpcNames.GET_UNIQUE_OBJECT_TYPES');
-            
+
             if (arg.status === Constants.SUCCESS_MESSAGE) {
                 //map received value to state object
                 this.mapDbObjectNamesToState(arg);
@@ -121,19 +122,19 @@ class DbStructureComponent extends React.Component {
             } else if (arg.status === Constants.FAILURE_MESSAGE) {
                 this.props.addMessageToNotificationsArray({ isPositive: false, text: arg.value });
             }
-            this.setState({isLoading: false});
+            this.setState({ isLoading: false });
         });
 
         ipcRenderer.on(rpcNames.GET_ALL_OBJECTS_BY_OBJECT_TYPE.respName, (event, arg) => {
             log.info('ipcRenderer.on(rpcNames.GET_ALL_OBJECTS_BY_OBJECT_TYPE');
-            
+
             if (arg.status === Constants.SUCCESS_MESSAGE) {
                 //map received value to state object
                 this.mapDbSubObjectsToState(arg);
             } else if (arg.status === Constants.FAILURE_MESSAGE) {
                 this.props.addMessageToNotificationsArray({ isPositive: false, text: arg.value });
             }
-            this.setState({isLoading: false});
+            this.setState({ isLoading: false });
         });
     }
 
@@ -150,8 +151,8 @@ class DbStructureComponent extends React.Component {
 
     }
 
-    mapDbObjectNamesToState(res){
-        log.info('mapDbObjectNamesToState',res);
+    mapDbObjectNamesToState(res) {
+        log.info('mapDbObjectNamesToState', res);
         let valueArray = res.value;
         let structureItemsArray = [];
         valueArray.map((value, i) => {
@@ -162,16 +163,16 @@ class DbStructureComponent extends React.Component {
                 itemValues: []
             });
         });
-        this.setState({structureItemsArray: structureItemsArray});
+        this.setState({ structureItemsArray: structureItemsArray });
     }
 
-    mapDbSubObjectsToState(res){
+    mapDbSubObjectsToState(res) {
         // log.info('mapDbSubObjectsToState ',res);
         const objectType = res.value.objectType;
         let valueArray = res.value.resultArray;
         let structureItemsArray = this.state.structureItemsArray;
         //we must find according object with object type provided by result [objectType]
-        let requiredArrayItem = structureItemsArray.find((currentValue) =>{
+        let requiredArrayItem = structureItemsArray.find((currentValue) => {
             return currentValue.itemName === objectType;
         });
 
@@ -181,12 +182,13 @@ class DbStructureComponent extends React.Component {
         valueArray.map((value, i) => {
             // log.info('value: ', value);
             itemValuesArray.push({
-                        name: value[0],
-                        valid: value[1]
-                    });
+                name: value[0],
+                valid: value[1],
+                active: false
+            });
         });
         // log.info('mapDbSubObjectsToState will put to state structureItemsArray: ', structureItemsArray);
-        this.setState({structureItemsArray: structureItemsArray});
+        this.setState({ structureItemsArray: structureItemsArray });
     }
 }
 
